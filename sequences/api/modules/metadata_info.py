7012b9f47d2e44e5e6ae382d41d60b588f5b66ad
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-04-06 00:00:19 +0530

    created new api for getting strain ids

--- sequences/api/modules/metadata_info.py
+++ sequences/api/modules/metadata_info.py
@@ -39,7 +39,6 @@ class UserMetadataInfoSerializer(CustomSerializer):
 			raise serializers.ValidationError({'message': 'User Inactive'})
 		raise serializers.ValidationError({'message': 'Invalid Credentials'})
 
-
 class UserMetadataInfoAPI(generics.GenericAPIView):
 	queryset                = Metadata_objects
 	serializer_class        = UserMetadataInfoSerializer
@@ -55,3 +54,43 @@ class UserMetadataInfoAPI(generics.GenericAPIView):
 			return Response({'message': 'Invalid Payload'}, status=status.HTTP_400_BAD_REQUEST)
 		return Response({'message': 'Not Authenticated'}, status=status.HTTP_400_BAD_REQUEST)
 
+class UserMetadataOnlyNameInfoSerializer(CustomSerializer):
+	class Meta:
+		model  = Metadata_Handler
+		fields = [
+			'user'
+		]
+
+	def validate(self, value):
+		user = self.context['request'].user
+		qs = User.objects.filter(Q(username__iexact=user) | Q(email__iexact=user)).distinct()
+		if(qs.count() == 1):
+			user_obj = qs.first()
+			if(user_obj.is_active):
+				metadata_qs = Metadata_Handler.objects.filter(Q(user=user_obj))
+				if(metadata_qs.count() > 0):
+					temp = []
+					for index,i in enumerate(metadata_qs):
+						temp.append(i.metadata)
+					return_dict = itertools.chain(*temp)
+					only_name = [i['Virus name'] for i in return_dict]
+					return only_name
+				return {'message': 'No Data Found'}
+			raise serializers.ValidationError({'message': 'User Inactive'})
+		raise serializers.ValidationError({'message': 'Invalid Credentials'})
+
+
+class UserMetadataOnlyNameInfoAPI(generics.GenericAPIView):
+	queryset                = Metadata_objects
+	serializer_class        = UserMetadataOnlyNameInfoSerializer
+	permission_classes      = []
+
+	def post(self, request, *args, **kwargs):
+		if(request.user.is_authenticated):
+			if(not bool(request.data)):
+				serializer = self.get_serializer(data = request.data)
+				if(serializer.is_valid()):
+					return Response(serializer.object)
+				return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+			return Response({'message': 'Invalid Payload'}, status=status.HTTP_400_BAD_REQUEST)
+		return Response({'message': 'Not Authenticated'}, status=status.HTTP_400_BAD_REQUEST)
