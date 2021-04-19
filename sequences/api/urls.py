421a61cac0dcaaf173b5e599b6caa53a4444d002
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-04-19 20:44:20 +0530

    added link for new api

--- sequences/api/urls.py
+++ sequences/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from .modules.upload import UserFileUploadHandlerAPI
+from .modules.download_info import UserDownloadInfoAPI
 from .modules.metadata_stats import UserMetadataStatsAPI
 from .modules.metadata_upload import UserMetadataUploadAPI
 from .modules.metadata_info import UserMetadataInfoAPI, UserMetadataOnlyNameInfoAPI
@@ -9,5 +10,6 @@ urlpatterns = [
     url(r'^file-upload/$', UserFileUploadHandlerAPI.as_view(), name='file-upload'),
     url(r'^metadata-stats/$', UserMetadataStatsAPI.as_view(), name='metadata-stats'),
     url(r'^metadata-upload/$', UserMetadataUploadAPI.as_view(), name='metadata-upload'),
+    url(r'^metadata-download/$', UserDownloadInfoAPI.as_view(), name='metadata-download'),
     url(r'^metadata-info-name/$', UserMetadataOnlyNameInfoAPI.as_view(), name='metadata-info-name'),
 ]
