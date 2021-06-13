abe0ed84b094dc536cc108858d2320e70a9a87c7
Animesh <animesh.workplace@gmail.com>
2021-06-14 00:09:51 +0530

    updated message

--- sequences/api/tasks.py
+++ sequences/api/tasks.py
@@ -87,11 +87,14 @@ def send_email_success(workflow_info):
 		link = ""
 		for i in search_files:
 			link = i.share_with_link(share_type='view').share_link
-		message = account.new_message()
-		# message.to.add(['aks1@nibmg.ac.in', 'nkb1@nibmg.ac.in', 'ap3@nibmg.ac.in', 'rezwanuzzaman.laskar@gmail.com'])
-		message.to.add(['aks1@nibmg.ac.in'])
-		message.subject = '📦 Report [ INSACOG DataHub ]'
-		html_content	= f"""
+		message1 = account.new_message()
+		message2 = account.new_message()
+		message1.to.add(['aks1@nibmg.ac.in'])
+		message2.to.add(['animesh.workplace@gmail.com'])
+		# message2.to.add(['nkb1@nibmg.ac.in', 'ap3@nibmg.ac.in', 'rezwanuzzaman.laskar@gmail.com'])
+		message1.subject = '📦 Report [ INSACOG DataHub ]'
+		message2.subject = '📦 Report [ INSACOG DataHub ]'
+		html_content1	= f"""
 			<div>
 				Dear all,
 					<p>
@@ -137,10 +140,55 @@ def send_email_success(workflow_info):
 					</p>
 			</div>
 		"""
-		message.body = html_content
+		html_content2	= f"""
+			<div>
+				Dear all,
+					<p>
+						This is an automated mail to provide the link for the report generated after the submission of
+						<strong style="background-color:#FFC748;text-decoration:none;">{ workflow_info['uploaded'] }
+						samples by { workflow_info['username'].split('_')[1] }</strong>.<br>
+						The pipeline has analyzed total
+						<strong style="background-color:#FFC748;text-decoration:none;">{ workflow_info['total_seq'] } sequences</strong>.
+					</p>
+					<p>
+						The link contains following files:
+						<ul>
+							<li>Raw metadata and sequences [ Combined ]</li>
+							<li>Aligned sequences using mafft</li>
+							<li>Nextstrain formatted metadata and sequences [ Ready to use ]</li>
+							<li>Combined metadata with all information in INSACOG DataHub metadata format</li>
+							<li>Clade and Lineage report</li>
+							<li>Mutation count report for all and State wise</li>
+							<li>Specific VoC/VoI ID report for all and State wise</li>
+							<li>Specific VoC/VoI progression report for all and State wise</li>
+							<li>Lineage deletion/substitution report</li>
+							<li>Logs</li>
+						</ul>
+					</p>
+					<p>
+						<table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:separate;line-height:100%;">
+						  <tr>
+							<td align="center" bgcolor="#19cca3" role="presentation" style="border:none;border-radius:6px;cursor:auto;padding:11px 20px;background:#19cca3;" valign="middle">
+							  <a href="{link}" target="_blank"
+							  		style="background:#19cca3;color:#ffffff;font-family:Helvetica, sans-serif;font-size:18px;font-weight:600;line-height:120%;Margin:0;text-decoration:none;text-transform:none;">
+								Click here!
+							  </a>
+							</td>
+						  </tr>
+						</table>
+					</p>
+					<p>
+						With Regards,<br>
+						INSACOG DataHub
+					</p>
+			</div>
+		"""
+		message1.body = html_content1
+		message2.body = html_content2
 		try:
-			response = message.send()
-			if(response):
+			response1 = message1.send()
+			response2 = message2.send()
+			if(response1 and response2):
 				return "Mail sent"
 			return "Mail couldn\'t be sent"
 		except Exception as e:
