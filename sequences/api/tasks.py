93fd266aafeca956110b06d6c4859139b7e03b13
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-09-26 12:16:36 +0530

    removed tasks not required

--- sequences/api/tasks.py
+++ sequences/api/tasks.py
@@ -22,9 +22,6 @@ from django.conf import settings
 from django.utils import timezone
 from zipfile import ZipFile, ZIP_DEFLATED
 from sequences.models import Download_Handler, Metadata_Handler, Frontend_Handler
-# from sendgrid import SendGridAPIClient
-# from .ssh_job_submission import RemoteClient
-# from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 
 load_dotenv(os.path.join(settings.BASE_DIR, '.env'))
 
@@ -55,10 +52,11 @@ def get_my_metadata(user_obj):
 def get_dashboard(user_obj):
 	frontend_obj = Frontend_Handler.objects.last()
 	dashboard = {
+		"map_data": frontend_obj.map_data,
+		"states_covered": int(frontend_obj.states_covered),
 		"genomes_sequenced": int(frontend_obj.genomes_sequenced),
 		"variants_catalogued": int(frontend_obj.variants_catalogued),
 		"lineages_catalogued": int(frontend_obj.lineages_catalogued),
-		"states_covered": int(frontend_obj.states_covered),
 	}
 	return dashboard
 
@@ -72,22 +70,6 @@ def get_all_metadata(each_page, page):
 		"total_length": math.ceil(len(frontend_obj.metadata)/each_page)
 	}
 	return data
-
-def get_map_data(user_obj):
-	frontend_obj = Frontend_Handler.objects.last()
-	return frontend_obj.map_data
-
-def get_bar_chart_data(user_obj):
-	frontend_obj = Frontend_Handler.objects.last()
-	return frontend_obj.bar_chart_data
-
-def get_treemap_chart_data(user_obj):
-	frontend_obj = Frontend_Handler.objects.last()
-	return frontend_obj.treemap_chart_data
-
-def get_lineage_definition_data(user_obj):
-	frontend_obj = Frontend_Handler.objects.last()
-	return frontend_obj.lineage_definition_data
 
 def create_download_link(workflow_info):
 	download_link = f"{os.getenv('DOWNLOAD_URL')}/INSACOG_data_{workflow_info['upload_time']}.zip"
@@ -123,104 +105,19 @@ def create_frontend_entry(self, workflow_info):
 	genes = ['E', 'M', 'N', 'ORF1a', 'ORF1b', 'ORF3a', 'ORF6', 'ORF7a', 'ORF7b', 'ORF8', 'ORF9b', 'S']
 	treemap_chart_data['genes'] = genes
 
-	for (key,value) in dict(collections.Counter(workflow_df['District'].tolist())).items():
+	for (key,value) in dict(collections.Counter(workflow_df['State'].tolist())).items():
 		map_data.append({
 			"name": key,
 			"value": value
 		})
 
-	bar_chart_data['xAxis'] = {
-		"month": month_keys
-	}
-	bar_chart_data['India'] = {}
-	for i in pandas.unique(workflow_df['lineage']).tolist():
-		lineage_metadata = workflow_df.iloc[workflow_df.index[workflow_df['lineage'].isin([i])].tolist()]
-		lineage_count = len(lineage_metadata)
-		temp = []
-		for j in lineage_metadata['aaSubstitutions']:
-			if(isinstance(j, str)):
-				temp.append(j.split(','))
-		aaSubstitution = list(itertools.chain(*temp))
-		lineage_definition_data[i] = [key for index,key in enumerate(aaSubstitution) if(index<20)]
-		bar_chart_data['India'][i] = {}
-		bar_chart_data['India'][i]['name'] = i
-		lineage_month = dict(collections.Counter(lineage_metadata['Collection month']))
-		for j in month_keys:
-			if(not j in list(lineage_month.keys())):
-				lineage_month[j] = 0
-		bar_chart_data['India'][i]['month'] = [lineage_month[i] for i in month_keys]
-
-	treemap_chart_data['India'] = {}
-	temp = []
-	for i in workflow_df['aaSubstitutions']:
-		if(isinstance(i, str)):
-			temp.append(i.split(','))
-	aaSubstitution = list(itertools.chain(*temp))
-
-	for i in genes:
-		gene_aaSubstitution = []
-		treemap_chart_data['India'][i] = {}
-		for j in aaSubstitution:
-			if(j.startswith(f'{i}')):
-				gene_aaSubstitution.append(j)
-		gene_subsitution_counter = dict(collections.Counter(gene_aaSubstitution))
-		treemap_chart_data['India'][i]['name'] = i
-		treemap_chart_data['India'][i]['children'] = []
-		for index,value in gene_subsitution_counter.items():
-			treemap_chart_data['India'][i]['children'].append({
-				"name": index,
-				"value": value
-			})
-
-	for i in pandas.unique(workflow_df['State']).tolist():
-		state_metadata = workflow_df.iloc[workflow_df.index[workflow_df['State'].isin([i])].tolist()]
-		state_metadata.reset_index(drop = True, inplace = True)
-		treemap_chart_data[i] = {}
-		temp = []
-		for j in state_metadata['aaSubstitutions']:
-			if(isinstance(j, str)):
-				temp.append(j.split(','))
-		aaSubstitution = list(itertools.chain(*temp))
-		for j in genes:
-			gene_aaSubstitution = []
-			treemap_chart_data[i][j] = {}
-			for k in aaSubstitution:
-				if(k.startswith(f'{j}')):
-					gene_aaSubstitution.append(k)
-			gene_subsitution_counter = dict(collections.Counter(gene_aaSubstitution))
-			treemap_chart_data[i][j]['name'] = j
-			treemap_chart_data[i][j]['children'] = []
-			for index,value in gene_subsitution_counter.items():
-				treemap_chart_data[i][j]['children'].append({
-					"name": index,
-					"value": value
-				})
-
-	for i in pandas.unique(workflow_df['State']).tolist():
-		state_metadata = workflow_df.iloc[workflow_df.index[workflow_df['State'].isin([i])].tolist()]
-		state_metadata.reset_index(drop = True, inplace = True)
-		bar_chart_data[i] = {}
-		for j in pandas.unique(state_metadata['lineage']).tolist():
-			lineage_metadata = state_metadata.iloc[state_metadata.index[state_metadata['lineage'].isin([j])].tolist()]
-			bar_chart_data[i][j] = {}
-			bar_chart_data[i][j]['name'] = j
-			lineage_month = dict(collections.Counter(lineage_metadata['Collection month']))
-			for k in month_keys:
-				if(not k in list(lineage_month.keys())):
-					lineage_month[k] = 0
-			bar_chart_data[i][j]['month'] = [lineage_month[month] for month in month_keys]
-
 	download_obj = Frontend_Handler(
 		map_data = map_data,
-		bar_chart_data = bar_chart_data,
 		states_covered = states_covered,
-		unique_variants = unique_variants,
 		metadata = workflow_info["message"],
 		genomes_sequenced = genomes_sequenced,
-		treemap_chart_data = treemap_chart_data,
 		variants_catalogued = variants_catalogued,
 		lineages_catalogued = lineages_catalogued,
-		lineage_definition_data = lineage_definition_data,
 	)
 	download_obj.save()
 
@@ -418,148 +315,3 @@ def send_email_error(workflow_info):
 			return "Mail couldn\'t be sent"
 	else:
 		print('Authentication not done')
-
-# def send_email_upload(user_info):
-# 	mailing_list = [
-# 		('aks1@nibmg.ac.in', 'Animesh Kumar Singh'),
-# 		# ('nkb1@nibmg.ac.in', 'Dr. Nidhan Biswas'),
-# 		# ('ap3@nibmg.ac.in', 'Antara Paul'),
-# 		# ('rezwanuzzaman.laskar@gmail.com', 'Rezwanuzzaman Laskar')
-# 	]
-# 	message = Mail(
-# 		from_email		= os.getenv('SENDGRID_EMAIL'),
-# 		to_emails		= mailing_list,
-# 		subject 		= '✅|📤 Upload Info [ INSACOG DataHub Test Hub ]',
-# 		html_content	= f"""
-# 			<div>
-# 				Dear all,
-# 					<p>
-# 						This is an automated mail to alert you of the submission of
-# 						<strong style="background-color:#FFC748;">{ user_info['uploaded'] } samples by
-# 						{ user_info['username'].split('_')[1] }</strong>.
-# 						The pipeline to generate report has been activated and
-# 						soon you will get another mail with all the reports.
-# 					</p>
-# 					<p>
-# 					With Regards,<br>
-# 					INSACOG DataHub
-# 					</p>
-# 			</div>
-# 		"""
-# 		)
-# 	try:
-# 		sg = SendGridAPIClient(os.getenv('SENDGRID_API'))
-# 		response = sg.send(message)
-# 	except Exception as e:
-# 		error_traceback = traceback.format_exc()
-# 		print(error_traceback)
-# 		return "Mail couldn\'t be sent"
-
-# 	return "Mail Sent"
-
-
-# def send_email_success(workflow_info):
-# 	start_time = pendulum.from_format(workflow_info["upload_time"], "YYYY-MM-DD_hh-mm-ss-A", tz="Asia/Kolkata")
-# 	end_time = pendulum.from_format(workflow_info["timestamp"], "YYYY-MM-DD_hh-mm-ss-A", tz="Asia/Kolkata")
-# 	mailing_list = [
-# 		('aks1@nibmg.ac.in', 'Animesh Kumar Singh'),
-# 		# ('nkb1@nibmg.ac.in', 'Dr. Nidhan Biswas'),
-# 		# ('ap3@nibmg.ac.in', 'Antara Paul'),
-# 		# ('rezwanuzzaman.laskar@gmail.com', 'Rezwanuzzaman Laskar')
-# 	]
-# 	message = Mail(
-# 		from_email		= os.getenv('SENDGRID_EMAIL'),
-# 		to_emails		= mailing_list,
-# 		subject 		= '📦 Report [ INSACOG DataHub Test Hub ]',
-# 		html_content	= f"""
-# 			<div>
-# 				Dear all,
-# 					<p>
-# 						This is an automated mail to provide the link for the report generated after the submission of
-# 						<strong style="background-color:#FFC748;">{ workflow_info['uploaded'] }
-# 						samples by { workflow_info['username'].split('_')[1] }</strong>.<br>
-# 						The pipeline has analyzed total
-# 						<strong style="background-color:#FFC748;">{ workflow_info['total_seq'] } sequences</strong> and took
-# 						<strong style="background-color:#FFC748;">{ end_time.diff(start_time).in_minutes() } minutes</strong> starting at
-# 						<strong style="background-color:#FFC748;">{ start_time.to_day_datetime_string() }</strong> and completing at
-# 						<strong style="background-color:#FFC748;">{ end_time.to_day_datetime_string() }</strong> to generate reports.
-# 					</p>
-
-# 					<p>
-# 						The link contains following files:
-# 						<ul>
-# 							<li>Raw metadata and sequences [ Combined ]</li>
-# 							<li>Aligned sequences using mafft</li>
-# 							<li>Nextstrain formatted metadata and sequences [ Ready to use ]</li>
-# 							<li>Combined metadata with all information in INSACOG DataHub metadata format</li>
-# 							<li>Clade and Lineage report</li>
-# 							<li>Mutation count report for all and State wise</li>
-# 							<li>Specific VoC/VoI ID report for all and State wise</li>
-# 							<li>Specific VoC/VoI progression report for all and State wise</li>
-# 							<li>Lineage deletion/substitution report</li>
-# 							<li>Logs</li>
-# 						</ul>
-# 					</p>
-
-# 					<p>
-# 						With Regards,<br>
-# 						INSACOG DataHub
-# 					</p>
-# 			</div>
-# 		"""
-# 		)
-# 	try:
-# 		sg = SendGridAPIClient(os.getenv('SENDGRID_API'))
-# 		response = sg.send(message)
-# 	except Exception as e:
-# 		error_traceback = traceback.format_exc()
-# 		print(error_traceback)
-# 		return "Mail couldn\'t be sent"
-
-# 	return "Mail Sent"
-
-# def send_email_error(workflow_info):
-# 	start_time = pendulum.from_format(workflow_info["upload_time"], "YYYY-MM-DD_hh-mm-ss-A", tz="Asia/Kolkata")
-# 	end_time = pendulum.from_format(workflow_info["timestamp"], "YYYY-MM-DD_hh-mm-ss-A", tz="Asia/Kolkata")
-# 	mailing_list = [
-# 		('aks1@nibmg.ac.in', 'Animesh Kumar Singh'),
-# 	]
-# 	message = Mail(
-# 		from_email		= os.getenv('SENDGRID_EMAIL'),
-# 		to_emails		= mailing_list,
-# 		subject 		= f'☠️🆘☠️ Error Information [ INSACOG DataHub Test Hub ]',
-# 		html_content	= f"""
-# 			<div>
-# 				Dear Animesh Kumar Singh,
-# 					<p>
-# 						This is an automated mail to alert you of an error that occured during the analysis
-# 						and report generation which was started after the submission of
-# 						<strong style="background-color:#FFC748;">{ workflow_info['uploaded'] } samples by
-# 						{ workflow_info['username'].split('_')[1] }</strong>. The workflow started at
-# 						<strong style="background-color:#FFC748;">{ start_time.to_day_datetime_string() }</strong> and error occured at
-# 						<strong style="background-color:#FFC748;">{ end_time.to_day_datetime_string() }</strong>.
-# 					</p>
-
-# 					<p>
-# 						Traceback of the error which occured in the step <strong style="background-color:#FFC748;">{ workflow_info['tool'] }</strong>
-# 					</p>
-
-# 					<hr>
-# 						<pre style="font-family:'Open Sans';background-color:#E5F1DC;">{ workflow_info['message'] }</pre>
-# 					<hr>
-
-# 					<p>
-# 					With Regards,<br>
-# 					INSACOG DataHub
-# 					</p>
-# 			</div>
-# 		"""
-# 		)
-# 	try:
-# 		sg = SendGridAPIClient(os.getenv('SENDGRID_API'))
-# 		response = sg.send(message)
-# 		return "Mail sent"
-# 	except Exception as e:
-# 		print(e.message)
-# 		return "Mail couldn\'t be sent"
-
