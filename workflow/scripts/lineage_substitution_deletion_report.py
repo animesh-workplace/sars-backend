24ffbc2bfc391776c09386a50ece1e04899eb1dd
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-10-27 15:52:14 +0530

    added entries where aasubtitutions and aadeletion is not present

--- workflow/scripts/lineage_substitution_deletion_report.py
+++ workflow/scripts/lineage_substitution_deletion_report.py
@@ -19,13 +19,22 @@ for i in metadata.index:
 		all_changes.append(metadata.iloc[i]['aaSubstitutions'].split(','))
 	if(isinstance(metadata.iloc[i]['aaDeletions'], str)):
 		all_changes.append(metadata.iloc[i]['aaDeletions'].split(','))
-	all_changes = list(itertools.chain(*all_changes))
-	p = [new_dict.append({
-			'date': metadata.iloc[i]['date'],
-			'strain': metadata.iloc[i]['strain'],
-			'state': metadata.iloc[i]['division'],
-			'lineage':	metadata.iloc[i]['lineage'],
-			'mutation/deletion': j,
-		}) for j in all_changes]
+	if(len(all_changes) > 0):
+		all_changes = list(itertools.chain(*all_changes))
+		p = [new_dict.append({
+				'date': metadata.iloc[i]['date'],
+				'strain': metadata.iloc[i]['strain'],
+				'state': metadata.iloc[i]['division'],
+				'lineage':	metadata.iloc[i]['lineage'],
+				'mutation/deletion': j,
+			}) for j in all_changes]
+	else:
+		p = new_dict.append({
+				'date': metadata.iloc[i]['date'],
+				'strain': metadata.iloc[i]['strain'],
+				'state': metadata.iloc[i]['division'],
+				'lineage':	metadata.iloc[i]['lineage'],
+				'mutation/deletion': '',
+			})
 
 pandas.DataFrame.from_dict(data=new_dict, orient='columns').to_csv(output_url, sep='\t', header=True, index=False)
