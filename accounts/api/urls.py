21fdbd753cb23248d1a88f29cbe31b56abdde884
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-10-28 06:39:35 +0530

    url updated to path

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
 from .modules.login import LoginAPIView
 from .modules.user_info import InfoAPIView
 from .modules.register import RegisterAPIView
+from django.urls import path, include, re_path
 from .modules.edit_password import PasswordUpdateAPIView
 from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token
 
 urlpatterns = [
-	url(r'^jwt/$', obtain_jwt_token),
-	url(r'^jwt/refresh/$', refresh_jwt_token),
-	url(r'^login/$', LoginAPIView.as_view(), name='login'),
-	url(r'^user-info/$', InfoAPIView.as_view(), name='user-info'),
-	url(r'^register/$', RegisterAPIView.as_view(), name='register'),
-	url(r'^update-password/$', PasswordUpdateAPIView.as_view(), name='update-password'),
+	path('jwt/', obtain_jwt_token),
+	path('jwt/refresh/', refresh_jwt_token),
+	path('login/', LoginAPIView.as_view(), name='login'),
+	path('user-info/', InfoAPIView.as_view(), name='user-info'),
+	path('register/', RegisterAPIView.as_view(), name='register'),
+	path('update-password/', PasswordUpdateAPIView.as_view(), name='update-password'),
 ]
