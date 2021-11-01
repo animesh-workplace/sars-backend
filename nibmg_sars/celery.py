9b46280cfc667fc687c79156b63654164cf41964
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-01 15:45:48 +0530

    removed unnecessary code

--- nibmg_sars/celery.py
+++ nibmg_sars/celery.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from django.apps import apps
@@ -9,7 +8,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nibmg_sars.settings')
 app = Celery('nibmg_sars')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-# app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 
 @app.task(bind=True)
 def debug_task(self):
