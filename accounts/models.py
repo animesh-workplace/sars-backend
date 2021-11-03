ab443a943986ba3615453cea4d80a9bcedba8073
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-03 09:45:35 +0530

    added download to model

--- accounts/models.py
+++ accounts/models.py
@@ -9,10 +9,11 @@ load_dotenv(os.path.join(settings.BASE_DIR, '.env'))
 
 # Create your models here.
 class Accounts(AbstractUser):
-	first_name 		= models.CharField(max_length=255, blank=True)
-	last_name 		= models.CharField(max_length=255, blank=True)
-	institute		= models.CharField(blank=True, max_length=500)
 	state			= models.CharField(blank=True, max_length=255)
 	city			= models.CharField(blank=True, max_length=255)
 	address			= models.CharField(blank=True, max_length=1000)
+	download 		= models.BooleanField(default=False, verbose_name='Download status')
 	pin_code		= models.DecimalField(blank=True, null=True, max_digits=10, decimal_places=0)
+	last_name 		= models.CharField(max_length=255, blank=True)
+	institute		= models.CharField(blank=True, max_length=500)
+	first_name 		= models.CharField(max_length=255, blank=True)
