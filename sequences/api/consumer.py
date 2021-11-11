d0c0abff1b29de31c4acffaf66ebc767404e42b4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-11 10:10:07 +0530

    removed all_metadata and search_metadata option
    Description: using database_sync_to_async and calling another function inside the function is prohibited and fetches an error

--- sequences/api/consumer.py
+++ sequences/api/consumer.py
@@ -33,16 +33,6 @@ class FrontendConsumer(AsyncJsonWebsocketConsumer):
 				"type": "MY_METADATA",
 				"data": await get_my_metadata(self.scope["user"], event["filter"]["each_page"], event["filter"]["page"], search)
 			}
-		elif(event["type"] == "ALL_METADATA"):
-			result = {
-				"type": "ALL_METADATA",
-				"data": await get_all_metadata(event["filter"]["each_page"], event["filter"]["page"])
-			}
-		elif(event["type"] == "SEARCH_METADATA"):
-			result = {
-				"type": "SEARCH_METADATA",
-				"data": await search_my_metadata(self.scope["user"], event["filter"]["search"])
-			}
 		else:
 			result = {
 				"type": "ERROR"
