449857fec07b91645de995c3f89df76a7cfe3df4
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-22 19:01:19 +0530

    removed unnecessary urls

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -6,10 +6,6 @@ from .modules.edit_password import PasswordUpdateAPIView
 from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token
 
 urlpatterns = [
-	path('jwt/', obtain_jwt_token),
-	path('jwt/refresh/', refresh_jwt_token),
 	path('login/', LoginAPIView.as_view(), name='login'),
 	path('user-info/', InfoAPIView.as_view(), name='user-info'),
-	path('register/', RegisterAPIView.as_view(), name='register'),
-	path('update-password/', PasswordUpdateAPIView.as_view(), name='update-password'),
 ]
