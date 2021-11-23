b3e0a90d36aa7ca2364753c2a040ae2c35c1601d
Animesh Kumar Singh <animesh.workplace@gmail.com>
2021-11-23 13:41:22 +0530

    removed unnecessary urls

--- sequences/api/urls.py
+++ sequences/api/urls.py
@@ -8,11 +8,8 @@ from .modules.metadata_info import UserMetadataInfoAPI, UserMetadataOnlyNameInfo
 
 urlpatterns = [
     path('landing-stats/', LandingStatsAPI.as_view(), name='landing-stats'),
-    path('metadata-info/', UserMetadataInfoAPI.as_view(), name='metadata-info'),
     path('file-upload/', UserFileUploadHandlerAPI.as_view(), name='file-upload'),
-    path('metadata-stats/', UserMetadataStatsAPI.as_view(), name='metadata-stats'),
     path('metadata-upload/', UserMetadataUploadAPI.as_view(), name='metadata-upload'),
     path('metadata-download/', UserDownloadInfoAPI.as_view(), name='metadata-download'),
     path('metadata-info-name/', UserMetadataOnlyNameInfoAPI.as_view(), name='metadata-info-name'),
-    path('metadata-stats-state/', UserMetadataStateStatsAPI.as_view(), name='metadata-stats-state'),
 ]
